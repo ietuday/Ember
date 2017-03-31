7fbc5ea6f3121c6de2e4ba03f00d8a890e65b794
Vaishali <vaishali.gaikwad@ridesoftech.com>
2017-03-31 12:21:08 +0530

    First ember commit

---
+++ app/controllers/rentals/index.js
@@ -0,0 +1,17 @@
+import Ember from 'ember';
+//
+export default Ember.Controller.extend({
+//actions: {
+//  filterByCity(param) {
+//    if (param !== '') {
+//      return this.get('store').query('rental', { city: param });
+//    } else {
+//      return this.get('store').findAll('rental');
+//    }
+//  }
+//}
+});
+
+// import RentalsController from '../rentals';
+//
+// export default RentalsController;
---
+++ app/routes/index.js
@@ -0,0 +1,25 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+
+  beforeModel() {
+  this.replaceWith('rentals');
+}
+
+// model() {
+//   return this.get('store').findAll('rental');
+// }
+
+});
+
+
+// export default Ember.Route.extend({
+//
+//   //   beforeModel() {
+//   //   this.replaceWith('rentals');
+//   // }
+//
+//   model() {
+//     return this.get('store').findAll('rental');
+//   }
+// });
